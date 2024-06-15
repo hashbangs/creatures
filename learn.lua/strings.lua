67a9117e6535124675b03568fc98542263315b6d
Solara <solara.at@proton.me>
2024-06-16 01:23:25 +0200

    2.4 - Strings

---
+++ learn.lua/strings.lua
@@ -0,0 +1,10 @@
+local a, b
+
+a = "one string"
+b = string.gsub(a, "one", "another") -- change string parts
+print(a)
+print(b)
+print(tonumber("10"))
+print(type(tonumber("10")))
+print("\n")
+print("hello" + 1)
